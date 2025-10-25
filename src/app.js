3eceff349f7414fffe20c4600a5c11fb04107890
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-10-24 22:58:38 -0300

    feat: initialize server

---
+++ src/app.js
@@ -0,0 +1,24 @@
+// ponto de entrada da aplicação
+
+require('dotenv').config()
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const autoRoutes = require('./routes/auth')
+const movieRoutes = require('./routes/movies')
+
+const app = express()
+app.use(bodyParser.json())
+
+// Rota pública
+app.use('/login', autoRoutes)
+
+// Rotas privadas
+app.use('/movies', movieRoutes)
+
+// inicia o servidor
+const PORTA = process.env.PORT;
+app.listen(PORTA, () => {
+    console.log('API rodando na porta 3000')
+})
