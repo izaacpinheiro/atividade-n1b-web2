c780ce1dfa8746709db11528fba08f246bdd9be9
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-11-23 18:49:23 -0300

    feat: modifies CRUD functions

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,74 +1,86 @@
 // responsável por gerenciar as rotas do CRUD e de geração do PDF
 
 const express = require("express");
-const { movies } = require("../data/movies");
-const { v4: uuidv4 } = require("uuid");
+const Filme = require("../models/Filme")
 const authMiddleware = require("../middleware/auth");
 const weekdayMiddleware = require("../middleware/weekday");
-const { generatePDF } = require("../utils/pdfGenerator");
 
+// router de filmes
 const router = express.Router();
 
 // aplica a autentição de dia da semana e do JWT
 router.use(weekdayMiddleware);
 router.use(authMiddleware);
 
-// rota GET /moveis -> lista de filmes
-router.get("/", (req, res) => {
-    res.json(movies);
+// rota GET /filmes/listar -> lista de filmes
+router.get("/listar", async (req, res) => {
+    try {
+        const filmesBuscados = await Filme.find();
+        res.json({filmes: filmesBuscados})
+    } catch (error) {
+        res.json({error: true, mensagem: 'Erro durante busca dos filmes.', tipo: error})
+    }
 });
 
-// rota POST /movies -> adicionar filme a lista
-router.post("/", (req, res) => {
-    const { title, nota, code } = req.body;
-    if (!title || !nota || !code)
-        return res
-            .status(400)
-            .json({ error: "Titúlo, Nota e Código são obrigatórios" });
-
-    // conferindo se já existe um filme com o mesmo código
-    const exists = movies.find((m) => m.code === code);
-    if (exists)
-        return res
-            .status(409)
-            .json({ error: "Já existe um filme com esse código" });
-
-    // criando o novo filme
-    const newMovie = {
-        id: uuidv4(),
-        title,
-        nota,
-        code,
-    };
-    movies.push(newMovie);
-    res.status(201), res.json(newMovie);
+// rota POST /filmes/inserir -> adicionar filme a lista
+router.post("/inserir", async (req, res) => {
+    try {
+        const {titulo, nota, code} = req.body;
+        await Filme.create({titulo: titulo, nota: nota, code: code})
+        res.json({mensagem: 'Novo filme cadastrado.'})
+    } catch (error) {
+        res.json({error: true, mensagem: 'Erro durante cadastro do filme.', tipo: error});
+    }
 });
 
-// rota DELETE /filmes/:code
-router.delete("/:code", (req, res) => {
-    const { code } = req.params;
-    const index = movies.findIndex((m) => m.code === code);
-    if (index === -1)
-        return res.status(401).json({ error: "Filme não encontado" });
-
-    movies.splice(index, 1);
-    res.json({ message: 'Filme deletado.' });
+// rota DELETE /filmes/excluir/code -> excluir filme pelo código
+router.delete("/excluir/:code", async (req, res) => {
+    try {
+        const { code } = req.params;
+        await Filme.deleteOne({code: code});
+        res.json({mensagem: 'Exclusão realizada.'})
+    } catch (error) {
+        res.json({error: true, mensagem: 'Erro durante a exclusão.', tipo: error});
+    }
 });
 
-// rota GET /filmes/search?code -> busca pelo código
-router.get("/search", (req, res) => {
-    const { code } = req.query;
-    if (!code) return res.status(400).json({ error: "O código do filme é obrigatório para a pesquisa" });
-
-    const found = movies.filter((m) => m.code === code);
-    res.json(found);
+// rota GET /filmes/search/code -> buscar filme pelo código
+router.get("/search/:code", async (req, res) => {
+    try {
+        const filmeBuscado = await Filme.findOne({code: req.params.code});
+        res.json({filme: filmeBuscado})
+    } catch (error) {
+        res.json({error: true, mensagem: 'Erro durante a consulta.', tipo: error});
+    }
 });
 
-// rota GET /movies/pdf -> gera e retorna o PDF com os filmes
-router.get("/pdf", async (req, res) => {
-    res.setHeader("Content-Disposition", "attachment; filename=movies.pdf");
-    res.setHeader("Content-Type", "application/pdf");
-    generatePDF(movies, res);
-});
+// rota PUT /filmes/atualizar/code -> atualizar a nota de um filme
+router.put('/atualizar/:code', async (req, res) => {
+    try {
+        const { code } = req.params;
+        const { nota } = req.body;
+
+        // verifica se mandou a nota
+        if (nota === undefined) {
+            return res.status(400).json({error: "A nova nota é obrigatória."});
+        }
+
+        // atualiza o filme
+        const filmeAtualizado = await Filme.findOneAndUpdate(
+            { code },
+            { nota },
+            { new: true} // retorna o documento atualizado
+        );
+
+        // caso não tenha encontrado o filme
+        if (!filmeAtualizado) {
+            return res.status(404).json({ error: "Filme não encontrado." });
+        }
+
+        res.json({mensagem: "Nota atualizada.", filme: filmeAtualizado});
+    } catch (error) {
+        res.json({error: true, mensagem: 'Erro durante atualização da nota.', tipo: error});
+    }
+})
 
 module.exports = router;
