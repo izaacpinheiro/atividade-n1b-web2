9a997f9689eae787ae0edf6c378008d7b5da5793
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-12-02 20:33:02 -0300

    test: add all tests

--- src/test/filme.test.js
+++ src/test/filme.test.js
@@ -1,23 +1,48 @@
 const axios = require('axios')
 
-describe("Teste de rota GET /filmes/listar", () => {
-    test('Deve retornar status 200 e uma lista de filmes', async () => {
-        const result = await axios.get('http://localhost:3000/filmes/listar');
+describe("GET /filmes/listar", () => {
+    test("Deve retornar uma lista de filmes e status 200", async () => {
+        const result = await axios.get("http://localhost:3000/filmes/listar");
         expect(result.status).toBe(200);
         expect(result.data).toHaveProperty("filmes");
-        expect(Array.isArray(resposta.data.filmes)).toBe(true);
+        expect(Array.isArray(result.data.filmes)).toBe(true);
     });
 });
 
-describe("Teste de rota POST /filmes/inserir", () => {
-    test('Deve retornar status 200 e mensagem confirmando inserção.', async () => {
-        await axios.post('http://localhost:3000/filmes/inserir', {
-            "titulo": "A Bruxa de Blair",
-	        "nota": 10,
-	        "code": "mv5"
+describe("POST /filmes/inserir", () => {
+    test("Deve inserir um filme e status 200", async () => {
+        const result = await axios.post("http://localhost:3000/filmes/inserir", {
+            "titulo": "Filme Teste Jest",
+	        "nota": 1,
+	        "code": "jest1"
         });
-        const result = await axios.get('http://localhost:3000/filmes/search/mv5');
-        expect(result.data.filmes).toBeTruthy();
-        expect(result.data.filmes.titulo).toBeTruthy();
+        expect(result.status).toBe(200);
+        expect(result.data).toHaveProperty("mensagem");
+    });
+});
+
+describe("PUT /filmes/atualizar/:code", () => {
+    test("Deve atualizar a nota do filme e status 200", async () => {
+        const result = await axios.put("http://localhost:3000/filmes/atualizar/jest1", {
+            "nota": 10
+        });
+        expect(result.status).toBe(200);
+        expect(result.data).toHaveProperty("mensagem")
+    });
+});
+
+describe("DELETE /filmes/excluir/:code", () => {
+    test("Deve deletar um filme pelo code e status 200", async () => {
+        const result = await axios.delete("http://localhost:3000/filmes/excluir/jest1");
+        expect(result.status).toBe(200);
+        expect(result.data).toHaveProperty("mensagem")
+    });
+});
+
+describe("GET /filmes/search/:code", () => {
+    test("Deve retonar um filme e status 200", async () => {
+        const result = await axios.get("http://localhost:3000/filmes/search/mv1");
+        expect(result.status).toBe(200);
+        expect(result.data).toHaveProperty("filme")
     });
 });
