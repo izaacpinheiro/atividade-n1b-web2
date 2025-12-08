2c551eec8b568e0c1a574ebcb1d85e2fb49e278a
izaacpinheiro <izaacpinheiro.123@gmail.com>
2025-12-08 19:55:14 -0300

    docs: add comments

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,12 +2,14 @@ import multer from "multer";
 import multerS3 from "multer-s3";
 import { S3Config } from "../utils/S3Config.js";
 
+// middleware responsável por receber o arquivo e enviar para o S3
 export const upload = multer({
     storage: multerS3({
-        s3: S3Config,
+        s3: S3Config, // cliente S3 configurado
         bucket: process.env.AWS_BUCKET_NAME,
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key: (req, file, callabck) => {
+            // define o nome com pasta + timestamp
             const fileName = `filme/${Date.now()}-${file.originalname}`;
             callabck(null, fileName)
         },
--- src/utils/S3Config.js
+++ src/utils/S3Config.js
@@ -1,5 +1,6 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
+// configura e exporta o cliente S3 para fazer upload das imagens no AWS S3
 export const S3Config = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
